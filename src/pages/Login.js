33fbbf46b3e632082770bfcbc42558bf2f1efa6e
Vishwas <im.vishwas.03@gmail.com>
2023-04-26 17:28:30 +0530

    Initialized basic needed pages

---
+++ src/pages/Login.js
@@ -0,0 +1,14 @@
+import Base from "../components/Base";
+
+const Login=()=>{
+    return(
+        <Base>
+            <div>
+                <h1>This is login page.</h1>
+                <p>Welcome to login page.</p>
+            </div>
+        </Base>
+    );
+};
+
+export default Login;
---
+++ src/pages/Signup.js
@@ -0,0 +1,14 @@
+import Base from "../components/Base";
+
+const Signup=()=>{
+    return(
+        <Base>
+            <div>
+                <h1>This is signup page.</h1>
+                <p>Welcome to signup page.</p>
+            </div>
+        </Base>
+    );
+};
+
+export default Signup;
