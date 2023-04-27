28879b0d6c340f90d7f9f45706a372195d4784c7
Vishwas <im.vishwas.03@gmail.com>
2023-04-27 20:25:00 +0530

    Update Login.js
    Created login form.

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,47 @@
+import { Button, Card, CardBody, CardHeader, Col, Container, Form, FormGroup, Input, Label, Row } from "reactstrap";
 import Base from "../components/Base";
 
 const Login=()=>{
     return(
         <Base>
-            <div>
-                <h1>This is login page.</h1>
-                <p>Welcome to login page.</p>
-            </div>
+            <Container>
+                <Row className="mt-4">
+                    <Col sm={{size: 6, offset: 3 }}>
+                        <Card color="dark" inverse>
+                            <CardHeader>
+                                Fill information to login.
+                            </CardHeader>
+                            <CardBody>
+                                {/* creating form  */}
+                                <Form>
+                                    {/* username/email field  */}
+                                    <FormGroup>
+                                    <Label for="email">Enter Username</Label>
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        placeholder="Your username"
+                                    />
+                                    </FormGroup>
+                                    <FormGroup>
+                                    {/* password field  */}
+                                    <Label for="password">Enter Password</Label>
+                                    <Input
+                                        id="password"
+                                        type="password"
+                                        placeholder="Your "
+                                    />
+                                    </FormGroup>
+                                    <Container className="text-center">
+                                        <Button outline color="light">Login</Button>
+                                        <Button className="ms-2" color="secondary" type="reset">Reset</Button>
+                                    </Container>
+                                </Form>
+                            </CardBody>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
         </Base>
     );
 };
